2b09b4b6ddae0ec11af0d0e2cab44983d2305c99
Pranauv-Toddle <pranauv.kumar@toddleapp.com>
2025-09-21 12:33:35 +0530

    feat: added controller when both email and phone does not exist

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import express from "express";
 import _ from "lodash";
-import { getUsers, initDB } from "./database/init.js";
+import {
+  getUserByEmail,
+  getUserById,
+  getUserByPhone,
+  getUsers,
+  initDB,
+  insertUser,
+} from "./database/init.js";
 
 const app = express();
 
@@ -11,6 +18,43 @@ app.get("/", (req, res) => {
   const result = getUsers();
   console.log(result);
   res.status(200).json({ result, time: new Date().toISOString() });
+});
+
+const exists = (ele) => _.size(ele) > 0;
+
+app.post("/identify", (req, res) => {
+  const { email, phoneNumber } = req.body;
+
+  // console.log(req.body);
+
+  const emailContact = getUserByEmail({ email });
+  const phoneContact = getUserByPhone({ phone: phoneNumber });
+
+  console.log(emailContact, phoneContact);
+
+  // when both phone and email does not exist
+  if (!exists(emailContact) && !exists(phoneContact)) {
+    const newContact = insertUser({
+      phoneNumber,
+      email,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+      linkPrecedence: "primary",
+    });
+
+    // newContact contains the last inserted row's id
+    const contact = getUserById(newContact);
+    return res.json({
+      contact: {
+        primaryContatctId: newContact,
+        emails: [contact.email],
+        phoneNumbers: [contact.phoneNumber],
+        secondaryContactIds: [],
+      },
+    });
+  }
+
+  return res.status(200).send("identify request default response");
 });
 
 app.listen(5000, () => {
