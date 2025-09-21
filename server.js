9611c85152fbee0accd7c09f250c6d137f655214
Pranauv-Toddle <pranauv.kumar@toddleapp.com>
2025-09-21 12:50:55 +0530

    refactor: added controller when both phone and email contacts are found

--- server.js
+++ server.js
@@ -53,7 +53,165 @@ app.post("/identify", (req, res) => {
       },
     });
   } else if (exists(phoneContact) && exists(emailContact)) {
-    // to do
+    // here when both phone and email contact exists
+    let isPrimaryEmail = false;
+    let emailPhoneContacts;
+
+    // email contacts can be primary or secondary
+    emailPhoneContacts = _.filter(
+      emailContact,
+      (d) => d.linkPrecedence == "primary"
+    )[0];
+
+    if (emailPhoneContacts) {
+      isPrimaryEmail = true;
+    } else {
+      // take the first since everything is secondary
+      emailPhoneContacts = emailContact[0];
+    }
+
+    let isPrimaryPhone = false;
+    let phonePhoneContacts;
+
+    // phone contacts can be primary or secondary
+    phonePhoneContacts = _.filter(
+      phoneContact,
+      (d) => d.linkPrecedence == "primary"
+    )[0];
+
+    if (phonePhoneContacts) {
+      isPrimaryPhone = true;
+    } else {
+      // take the first since everything is secondary
+      phonePhoneContacts = phoneContact[0];
+    }
+
+    // console.log(phonePhoneContacts, emailPhoneContacts);
+
+    if (phonePhoneContacts.id !== emailPhoneContacts.id) {
+      // here when phone and email contacts are different
+
+      // console.log("inside different phone and email contact");
+      let newerContact = null;
+      if (phonePhoneContacts.createdAt <= emailPhoneContacts.createdAt) {
+        newerContact = emailPhoneContacts;
+      } else {
+        newerContact = phonePhoneContacts;
+      }
+
+      if (newerContact) {
+        // newerContact = latest contact (that needs to be updated)
+        // oldContact = oldest one (that will remain as it is)
+        const oldContact =
+          newerContact == phonePhoneContacts
+            ? emailPhoneContacts
+            : phonePhoneContacts;
+
+        // this part is basically to get the secondary contacts by the contact's id that we're supposed to update later down
+        const linkedId =
+          isPrimaryEmail && isPrimaryPhone
+            ? newerContact.id
+            : isPrimaryEmail && newerContact == emailPhoneContacts
+            ? newerContact.id
+            : isPrimaryPhone && newerContact == phonePhoneContacts
+            ? newerContact.id
+            : null;
+        let secondaryContacts = [];
+        if (linkedId) {
+          secondaryContacts = getUsersByLinkedId(newerContact.id);
+        }
+
+        // console.log(oldContact, "old contact");
+        // console.log(newerContact, "new contact");
+        // console.log(secondaryContacts, "secondary contacts");
+
+        // this part is where we get the new primary linked id from the oldContact - depending on if old contact is primary or secondary
+        let newLinkedId = null;
+        if (oldContact == emailPhoneContacts) {
+          if (isPrimaryEmail) newLinkedId = oldContact.id;
+          else newLinkedId = oldContact.linkedId;
+        } else if (oldContact == phonePhoneContacts) {
+          if (isPrimaryPhone) newLinkedId = oldContact.id;
+          else newLinkedId = oldContact.linkedId;
+        }
+
+        // get secondary contacts and update their linkedIds to the newer primary one
+        if (exists(secondaryContacts)) {
+          for (let contact of secondaryContacts) {
+            updateUser({
+              id: contact.id,
+              linkedId: newLinkedId,
+              linkPrecedence: "secondary",
+              email: contact.email,
+              phoneNumber: contact.phoneNumber,
+              updatedAt: new Date().toISOString(),
+            });
+          }
+        }
+
+        // then update the latest contact with the oldest one's linkedid based off of createdAt
+        const result = updateUser({
+          id: newerContact.id,
+          linkedId: newLinkedId,
+          linkPrecedence: "secondary",
+          email: newerContact.email,
+          phoneNumber: newerContact.phoneNumber,
+          updatedAt: new Date().toISOString(),
+        });
+
+        const newSecondaryContacts = getUsersByLinkedId(newLinkedId);
+
+        // console.log(newSecondaryContacts, "new secondary contacts");
+
+        const primaryContact = getUserById(newLinkedId);
+
+        return res.json({
+          contact: {
+            primaryContatctId: primaryContact.id,
+            emails: [
+              primaryContact.email,
+              ..._.uniq(_.map(newSecondaryContacts, (d) => d.email)),
+            ],
+            phoneNumbers: [
+              primaryContact.phoneNumber,
+              ..._.uniq(_.map(newSecondaryContacts, (d) => d.phoneNumber)),
+            ],
+            secondaryContactIds: _.map(newSecondaryContacts, (d) => d.id),
+          },
+        });
+      }
+    } else {
+      // both phone and email contact selected have the same id => same row
+      // console.log("inside same phone and email contact");
+
+      const linkedId =
+        isPrimaryEmail && isPrimaryPhone
+          ? emailPhoneContacts.id // any of phone and email contact works
+          : isPrimaryEmail
+          ? emailPhoneContacts.id
+          : isPrimaryPhone
+          ? phonePhoneContacts.id
+          : phonePhoneContacts.linkedId; // take any 1 of contact's linkedid
+
+      const secondaryContacts = getUsersByLinkedId(linkedId);
+
+      const primaryContact = getUserById(linkedId);
+
+      return res.json({
+        contact: {
+          primaryContatctId: primaryContact.id,
+          emails: [
+            primaryContact.email,
+            ..._.uniq(_.map(secondaryContacts, (d) => d.email)),
+          ],
+          phoneNumbers: [
+            primaryContact.phoneNumber,
+            ..._.uniq(_.map(secondaryContacts, (d) => d.phoneNumber)),
+          ],
+          secondaryContactIds: _.map(secondaryContacts, (d) => d.id),
+        },
+      });
+    }
   } else if (!exists(phoneContact) && emailContact) {
     // here inside when phone contact doesn't exist but email contact does
     let isPrimary = false;
